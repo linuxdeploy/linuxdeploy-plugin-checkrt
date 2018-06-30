db54b59bec27d8a1cf7f71e9ce663a757bea5c5f
TheAssassin <theassassin@assassinate-you.net>
2018-07-01 00:34:36 +0200

    Fix deployment of libc.so.6

--- linuxdeploy-plugin-checkrt.template.sh
+++ linuxdeploy-plugin-checkrt.template.sh
@@ -46,8 +46,8 @@ echo "Extracting binaries"
 dd if="$script" skip="$OFFSET" iflag=skip_bytes,count_bytes 2>/dev/null | tar -xz
 
 # copy system libraries
-for path in /usr/lib/x86_64-linux-gnu/libstdc++.so.6 /lib/x86_64-linux-gnu/libgcc_s.so.1 /lib/x86_64-linux-gnu/libm.so.6 lib/x86_64-linux-gnu/libc.so.6; do
-    if [ -f "$path" ]; then
+for path in /usr/lib/x86_64-linux-gnu/libstdc++.so.6 /lib/x86_64-linux-gnu/libgcc_s.so.1 /lib/x86_64-linux-gnu/libm.so.6 /lib/x86_64-linux-gnu/libc.so.6; do
+    if [ -e "$path" ]; then
         echo "Copying into AppDir: $path"
         mkdir -p usr/optional/$(basename "$path")
         cp "$path" usr/optional/$(basename "$path")
